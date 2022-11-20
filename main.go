c6e480476f5da158e18c11f24d637176f5b52596
Ilija Mihajlovic <iilija879a@gmail.com>
2022-11-20 12:09:04 +0100

    new line added

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func formHandler(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "ParseForm() err : %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request successful")
+	fmt.Fprintf(w, "POST request successful\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name = %s\n", name)
